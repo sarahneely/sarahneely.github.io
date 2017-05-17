b8d82edab594da5fc035a62870ab2c0d9b6ade9b
Sarah Neely <Sarah Neely>
2017-05-17 15:59:55 -0700

    commiting everything before I keep working

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.getElementById('about_btn').addEventListener('click', aboutPopUp)
 
 function aboutPopUp () {
-  document.getElementById('about_popup').style.visibility = visible;
+  document.getElementById('about_popup').style.visibility = 'visible';
+  console.log (aboutPopUp);
 }
