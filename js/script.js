dc21d043efa48f5cbcf20064e82c211aa015bd73
Sarah Neely <Sarah Neely>
2017-09-14 19:18:41 -0700

    added hover and js to contact section

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@ function aboutPopUp () {
   document.getElementById('about_popup').style.visibility = 'visible';
   console.log (aboutPopUp);
 }
+
+document.getElementById('mywork_btn').addEventListener('click', myWorkPopUp)
+
+function myWorkPopUp () {
+  document.getElementById('mywork_popup').style.visibility = 'visible';
+}
+
+document.getElementById('contact_btn').addEventListener('click', contactPopUp)
+
+function contactPopUp () {
+  document.getElementById('contact_popup').style.visibility = 'visible';
+}
+
+
+// var popUp () {
+//   if (aboutPopUp.style.visibility === 'visible' || myWorkPopUp.style.visibility === 'visible' || contactPopUp.style.visibility === 'visible') {
+//     document.addEventListener('click', hidePopup)
+//   }
+// }
+//
+// function hidePopup () {
+//   document.getElementById('about_popup' || 'mywork_popup' || 'contact_popup').style.visibility = 'hidden';
+// }
