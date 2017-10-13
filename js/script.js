5dd1f2133d09848357ff40f5d0aab60f7bfdb434
sarahneely <sporter1990@hotmail.com>
2017-10-13 16:40:36 -0700

    commiting changes to the about and my work popups. Improved js functionality

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-document.getElementById('about_btn').addEventListener('click', aboutPopUp)
+
+
+var aboutButton = document.getElementById('about_btn');
+aboutButton.addEventListener('click', aboutPopUp)
 
 function aboutPopUp () {
   document.getElementById('about_popup').style.visibility = 'visible';
   console.log (aboutPopUp);
 }
 
-document.getElementById('mywork_btn').addEventListener('click', myWorkPopUp)
+var myWork = document.getElementById('mywork_btn');
+myWork.addEventListener('click', myWorkPopUp);
 
 function myWorkPopUp () {
   document.getElementById('mywork_popup').style.visibility = 'visible';
 }
 
-document.getElementById('contact_btn').addEventListener('click', contactPopUp)
+var contactButton = document.getElementById('contact_btn');
+contactButton.addEventListener('click', contactPopUp);
 
 function contactPopUp () {
   document.getElementById('contact_popup').style.visibility = 'visible';
 }
 
-
-// var popUp () {
-//   if (aboutPopUp.style.visibility === 'visible' || myWorkPopUp.style.visibility === 'visible' || contactPopUp.style.visibility === 'visible') {
-//     document.addEventListener('click', hidePopup)
-//   }
-// }
-//
-// function hidePopup () {
-//   document.getElementById('about_popup' || 'mywork_popup' || 'contact_popup').style.visibility = 'hidden';
-// }
+var closeButton = document.getElementById('close_work');
+closeButton.addEventListener('click', closeWork);
+function closeWork () {
+    document.getElementById('mywork_popup').style.visibilty = 'hidden';
+}
